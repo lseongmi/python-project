65ab479e476d989ff8e9d9bf840ddf688ac84603
lseongmi <w2307@e-mirim.hs.kr>
2024-11-01 00:52:13 +0900

    Feat: popup 구현

--- js/list.js
+++ js/list.js
@@ -1,18 +1,13 @@
 const listpluscontainer = document.getElementById("list-plus-container");
-const listcontainer = document.getElementsByClassName("list-container")[0];
+const popup = document.getElementById("pop-up");
+const body = document.body; // body 요소 선택
 
 listpluscontainer.addEventListener('click', function() {
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("list-item");
-
-    newDiv.innerHTML = `
-        <div><img src="../image/plus (2).png" alt=""></div>
-        <div class="listtext-container">
-            <input type="text" class="list-text-input" placeholder="일정 내용을 입력하세요">
-            <input type="time" class="list-time-input">
-            <button class="submit-button">완료</button>
-        </div>
-    `;
-
-    listcontainer.appendChild(newDiv);
+    if (popup.style.display === "flex") {
+        popup.style.display = "none";
+        body.style.backgroundColor = ""; // 배경색 제거
+    } else {
+        popup.style.display = "flex";
+        body.style.backgroundColor = "rgba(0, 0, 0, 0.2)"; // 배경색 추가
+    }
 });
